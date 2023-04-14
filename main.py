eb516a7279b08e1900c7faaef581bd5b302af289
Jared Hawley <j.yelwah@gmail.com>
2023-04-14 12:39:56 -0400

    main update

--- main.py
+++ main.py
@@ -21,21 +21,4 @@ ally_team = {
   "support": ""
 }
 
-"""
-bans = ["","",""]
-enemy_team = {
-  "top":     "",
-  "jungle":  "",
-  "middle":  "",
-  "bottom":  "",
-  "support": ""
-}
-ally_team = {
-  "top":     "",
-  "jungle":  "",
-  "middle":  "",
-  "bottom":  "",
-  "support": ""
-}
-"""
 analysis.bestPick(my_role, bans, champ_pool, enemy_team, ally_team)
